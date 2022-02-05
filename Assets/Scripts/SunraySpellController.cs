2735ba46ea72456869a872e4756910c1f3384ef0
Kharalos <ounal97@gmail.com>
2022-02-06 00:24:42 +0300

    vscode

--- Assets/Scripts/SunraySpellController.cs
+++ Assets/Scripts/SunraySpellController.cs
@@ -4,19 +4,15 @@ using UnityEngine;
 
 public class SunraySpellController : MonoBehaviour
 {
-    public Animator circleAnimator, rayAnimator;
-    public GameObject spellCircle;
+
     void Start()
     {
-        circleAnimator.SetTrigger("circle");
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*Animation ass;
-        if (as)
-            rayAnimator.SetTrigger("ray");*/
+
 
     }
 }
